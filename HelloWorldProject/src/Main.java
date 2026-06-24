75ae3c01aca1fe580f6fe55e54a902a5ec62341e
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-06-24 08:48:47 -0400

    Starter content (create two methods + 1)

--- HelloWorldProject/src/Main.java
+++ HelloWorldProject/src/Main.java
@@ -1,19 +1,22 @@
 public class Main {
     static String classLevelVariable = "This is a class level variable";
     
-    // create two methods 'printName' and 'printAge'
-    // pass name and age as parameters
     public static void main(String[] args) {
         int age = 16;
         String name = "Jonathan";
+        String location = "Cincinnati, OH";
         printName(name);
         printAge(age);
+        printLocation(location);
     }
     public static void printName(String name) {
         System.out.println("Hello, " + name);
     }
     public static void printAge(int age) {
         System.out.println("You are " + age + " years old");
+    }
+    public static void printLocation(String location) { 
+        System.out.println("You are located in " + location);
     }
     public static void displayMessage(String message) { 
         System.out.println(message);
