367e279f0c9d8b1e6e9bdcefc13cfc81762e3abf
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-06-24 13:56:27 -0400

    [POM] FirstSeleniumProject pom.xml and Project

---
+++ FirstSeleniumProject/src/main/java/com/jschway/example/firstselenium/FirstSeleniumProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jschway.example.firstselenium;
+
+/**
+ *
+ * @author JonathanSaddler
+ */
+public class FirstSeleniumProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
