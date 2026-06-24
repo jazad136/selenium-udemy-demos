3b3098eaefd8eb574705da04ba0483f48a705da8
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-06-24 08:46:03 -0400

    Starter content (create two methods)

--- HelloWorldProject/src/Main.java
+++ HelloWorldProject/src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
     static String classLevelVariable = "This is a class level variable";
     
+    // create two methods 'printName' and 'printAge'
+    // pass name and age as parameters
     public static void main(String[] args) {
-        displayMessage("first custom message");
-        displayMessage(classLevelVariable);
+        int age = 16;
+        String name = "Jonathan";
+        printName(name);
+        printAge(age);
+    }
+    public static void printName(String name) { 
+        System.out.println("Hello, " + name);
+    }
+    public static void printAge(int age) { 
+        System.out.println("You are " + age + " years old");
     }
     public static void displayMessage(String message) { 
         System.out.println(message);
