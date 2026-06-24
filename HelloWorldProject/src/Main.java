9e66ed6ec03b9d44d4b8376741a98bf8abc27e81
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-06-24 06:55:36 -0400

    Starter content (Call class level variable)

--- HelloWorldProject/src/Main.java
+++ HelloWorldProject/src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     static String classLevelVariable = "This is a class level variable";
     
     public static void main(String[] args) {
-        
         displayMessage("first custom message");
+        displayMessage(classLevelVariable);
     }
     public static void displayMessage(String message) { 
         System.out.println(message);
