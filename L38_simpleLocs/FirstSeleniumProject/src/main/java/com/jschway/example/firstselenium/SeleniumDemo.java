b61dab111c623274b9dad47abbb527cd70822789
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-06-26 07:44:16 -0400

    Test out Byxpath and BycssSelector

--- L38_simpleLocs/FirstSeleniumProject/src/main/java/com/jschway/example/firstselenium/SeleniumDemo.java
+++ L38_simpleLocs/FirstSeleniumProject/src/main/java/com/jschway/example/firstselenium/SeleniumDemo.java
@@ -2,6 +2,7 @@ package com.jschway.example.firstselenium;
 
 import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,17 +15,36 @@ public class SeleniumDemo {
      */
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        
         driver.get("https://practicetestautomation.com/practice-test-login/");
         // WebElementLocators will be added here.
+        try { 
         WebElement usernameInputField = driver.findElement(By.id("username"));
+        WebElement usernameInputFieldXpath = driver.findElement(By.xpath("//input[@id='username']"));
+        WebElement usernameInputFieldCss = driver.findElement(By.cssSelector("input[id='username']"));
+        
         WebElement passwordInputField = driver.findElement(By.name("password"));
+        WebElement passwordInputFieldXpath = driver.findElement(By.xpath("//input[@name='password']"));
+        WebElement passwordInputFieldCss = driver.findElement(By.cssSelector("input[name='password']"));
+        
         WebElement submitButton = driver.findElement(By.className("btn"));
+        WebElement submitButtonFieldXPath = driver.findElement(By.xpath("//button[@id='submit']"));
+        WebElement submitButtonFieldCss = driver.findElement(By.cssSelector("button[id='submit']"));
+        WebElement submitButtonFieldCss2 = driver.findElement(By.cssSelector("button[id='submit'][class='btn']"));
+        WebElement submitButtonFieldCss3 = driver.findElement(By.cssSelector("#submit.btn"));
+        
         List<WebElement> inputFields = driver.findElements(By.tagName("input"));
         WebElement linkTextLocator = driver.findElement(By.linkText("Practice Test Automation."));
         WebElement passwordFieldBelowUsername = driver.findElement(RelativeLocator.with(By.tagName("input")).below(By.id("username")));
         WebElement privacyPolicyLink = driver.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.partialLinkText("Test Automation")));
-        driver.quit();
+        } catch(NoSuchElementException e) { 
+            throw e;
+        }
+        finally{
+            driver.quit();
+        }
     }
+    
     
     private static String chromeTest(String url) { 
         WebDriver driver = new ChromeDriver();
