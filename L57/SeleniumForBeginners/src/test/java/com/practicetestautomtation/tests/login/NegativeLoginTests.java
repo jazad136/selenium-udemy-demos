1c9b59566d684c4658e11f550ae76c8849bb985e
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-01 16:17:46 -0400

    Code-less NegativeLoginTests.java file

---
+++ L57/SeleniumForBeginners/src/test/java/com/practicetestautomtation/tests/login/NegativeLoginTests.java
@@ -0,0 +1,22 @@
+package com.practicetestautomtation.tests.login;
+
+import org.testng.annotations.Test;
+
+public class NegativeLoginTests {
+    
+    @Test
+    public void incorrectUsernameTest() { 
+//        Open page
+//        Type username incorrectUser into Username field
+//        Type password Password123 into Password field
+//        Push Submit button
+//        Verify error message is displayed
+//        Verify error message text is Your username is invalid!
+        
+    }
+    
+    @Test
+    public void incorrectPasswordTest() { 
+        
+    }
+}
