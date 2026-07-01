fd42cad72328154e3aac2c8aeb2f4f3613376c3e
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-01 16:41:01 -0400

    Finish incorrectPasswordTest() method

--- L57/SeleniumForBeginners/src/test/java/com/practicetestautomtation/tests/login/NegativeLoginTests.java
+++ L57/SeleniumForBeginners/src/test/java/com/practicetestautomtation/tests/login/NegativeLoginTests.java
@@ -43,10 +43,30 @@ public class NegativeLoginTests {
     @Test
     public void incorrectPasswordTest() { 
         // Open page
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        
         // Type username student into Username field
+        WebElement txtUsername = driver.findElement(By.id("username"));
+        txtUsername.sendKeys("student");
+        
         // Type password incorrectPassword into Password field
+        WebElement txtPassword = driver.findElement(By.id("password"));
+        txtPassword.sendKeys("incorrectPassword");
+        
         // Push Submit button
+        WebElement btnSubmit = driver.findElement(By.id("submit"));
+        btnSubmit.click();
+        
         // Verify error message is displayed
+        WebElement lblError = driver.findElement(By.id("error"));
+        Assert.assertTrue(lblError.isDisplayed());
+        
         // Verify error message text is Your password is invalid!
+        String actualErrorMessage = lblError.getText();
+        String expectedErrorMessage = "Your password is invalid!";
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+        
+        driver.quit();
     }
 }
