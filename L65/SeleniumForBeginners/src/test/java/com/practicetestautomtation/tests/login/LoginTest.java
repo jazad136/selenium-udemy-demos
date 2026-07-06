c6b0c184b476440f222f73a597ea83c159010475
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-06 15:57:28 -0400

    Shift all methods to LoginTest.java

--- L65/SeleniumForBeginners/src/test/java/com/practicetestautomtation/tests/login/LoginTest.java
+++ L65/SeleniumForBeginners/src/test/java/com/practicetestautomtation/tests/login/LoginTest.java
@@ -6,15 +6,53 @@ import java.util.NoSuchElementException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NegativeLoginTest {
+public class LoginTest {
+    @Test
+    public void testLoginFunctionality() {
+        // Open page
+//        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        // Type username student into Username field
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        usernameInput.sendKeys("student");
+
+        // Type password Password123 into Password field
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys("Password123");
+
+        // Push Submit button
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+        try { Thread.sleep(2000); } 
+        catch(InterruptedException e) { }
+       
+        // Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+        String expectedUrl = "https://practicetestautomation.com/logged-in-successfully/";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+        // Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        String expectedMessage = "Logged In Successfully";
+        String pageSource = driver.getPageSource();
+        Assert.assertTrue(pageSource.contains(expectedMessage));
+        
+        // Verify button Log out is displayed on the new page
+        WebElement logOutButton = driver.findElement(By.linkText("Log out"));
+        Assert.assertTrue(logOutButton.isDisplayed());
+        
+        driver.quit();
+    }
     
     public Wait<WebDriver> setupWait(WebDriver driver) { 
         return new FluentWait<>(driver)
@@ -22,6 +60,7 @@ public class NegativeLoginTest {
                 .pollingEvery(Duration.of(2000, ChronoUnit.MILLIS))
                 .ignoring(NoSuchElementException.class);
     }
+    
     @Test
     public void incorrectUsernameTest() { 
         // Open page
@@ -62,8 +101,9 @@ public class NegativeLoginTest {
     @Test
     public void incorrectPasswordTest() { 
         // Open page
-//        WebDriver driver = new ChromeDriver();
-        WebDriver driver = new SafariDriver();
+        WebDriver driver = new ChromeDriver();
+        Wait<WebDriver> wait = setupWait(driver);
+//        WebDriver driver = new SafariDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
         
         // Type username student into Username field
@@ -80,6 +120,7 @@ public class NegativeLoginTest {
         
         // Verify error message is displayed
         WebElement lblError = driver.findElement(By.id("error"));
+        wait.until(ExpectedConditions.visibilityOf(lblError));
         Assert.assertTrue(lblError.isDisplayed());
         
         // Verify error message text is Your password is invalid!
