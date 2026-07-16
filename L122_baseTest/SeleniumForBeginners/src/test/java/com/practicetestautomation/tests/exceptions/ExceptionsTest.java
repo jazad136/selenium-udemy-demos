e0bb8f439d843e35937e0e1f725130c6e6cdd5d8
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-16 14:19:14 -0400

    Replace top variables with BaseTest in LoginTest and ExceptionTest

--- L122_baseTest/SeleniumForBeginners/src/test/java/com/practicetestautomation/tests/exceptions/ExceptionsTest.java
+++ L122_baseTest/SeleniumForBeginners/src/test/java/com/practicetestautomation/tests/exceptions/ExceptionsTest.java
@@ -1,55 +1,15 @@
 package com.practicetestautomation.tests.exceptions;
 
+import com.practicetestautomation.tests.BaseTest;
 import java.time.Duration;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class ExceptionsTest {
-    
-    private WebDriver driver;
-    private Logger logger;
-    
-    @Parameters("browser")
-    @BeforeMethod(alwaysRun=true)
-    public void setUp(@Optional("chrome") String browser) {
-        logger = Logger.getLogger(ExceptionsTest.class.getName());
-        logger.setLevel(Level.INFO);
-        
-        logger.info("Running test in " + browser);
-        switch (browser.toLowerCase()) {
-            case "chrome": 
-                driver = new ChromeDriver();
-     break; case "firefox":
-                driver = new FirefoxDriver();
-     break; default: 
-            logger.warning("Configuration for " + browser + " is missing, so running tests in Chrome by default.");
-            driver = new ChromeDriver();
-     break;
-        }
-//        wait = setupWait(driver);
-   // Open page
-        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
-    }
-    
-    @AfterMethod(alwaysRun=true)
-    public void tearDown() { 
-        driver.quit();
-        logger.info("Browser is closed");
-    }
-    
+public class ExceptionsTest extends BaseTest {
     @Test
     public void testNoSuchElementException() { 
         // Click Add button
--- L122_baseTest/SeleniumForBeginners/src/test/java/com/practicetestautomation/tests/login/LoginTest.java
+++ L122_baseTest/SeleniumForBeginners/src/test/java/com/practicetestautomation/tests/login/LoginTest.java
@@ -1,13 +1,11 @@
 package com.practicetestautomation.tests.login;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
 import com.practicetestautomation.pageobjects.LoginPage;
 import com.practicetestautomation.pageobjects.SuccessfulLoginPage;
+import com.practicetestautomation.tests.BaseTest;
 import com.practicetestautomation.testsetup.ExtentReportManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -17,12 +15,8 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class LoginTest {
+public class LoginTest extends BaseTest {
     
-    private WebDriver driver;
-    private Logger logger;
-    private ExtentReports extent;
-    private ExtentTest loginReport;
     
     
     @Parameters("browser")
@@ -43,7 +37,7 @@ public class LoginTest {
      break;
         }
         extent = ExtentReportManager.getReporter();
-        loginReport = extent.createTest("Login Test");
+        testReport = extent.createTest("Login Test");
     }
     
     @AfterMethod(alwaysRun=true)
@@ -60,7 +54,7 @@ public class LoginTest {
     public void testLoginFunctionality() {
 //        ExtentTest loginReport = extent.createTest("testLoginFunctionality");
         // Open page
-        loginReport.info("Starting testLoginFunctionality");
+        testReport.info("Starting testLoginFunctionality");
         LoginPage loginPage = new LoginPage(driver);
         loginPage.visit();
         
@@ -68,7 +62,7 @@ public class LoginTest {
         // Type password Password123 into Password field
         // Push Submit button
         
-        loginReport.info("Type username, password, and click submit button");
+        testReport.info("Type username, password, and click submit button");
         SuccessfulLoginPage successfulLoginPage = loginPage.executeLogin("student", "Password123");
         successfulLoginPage.load();
 
@@ -79,8 +73,8 @@ public class LoginTest {
         
         // Verify button Log out is displayed on the new page
         Assert.assertTrue(successfulLoginPage.isLogoutButtonDisplayed());
-        loginReport.info("Verify the login functionality");
-        loginReport.pass("Test passed...");
+        testReport.info("Verify the login functionality");
+        testReport.pass("Test passed...");
         extent.flush();
     }
     @Parameters({"username", "password", "expectedErrorMessage", "testName"})
@@ -88,20 +82,20 @@ public class LoginTest {
     public void negativeLoginTest(String username, String password, String expectedErrorMessage, String testName) {
 //        ExtentTest loginReport = extent.createTest("negativeLoginTest-"+testName);
         // Open page
-        loginReport.info("Starting negativeLoginTest");
+        testReport.info("Starting negativeLoginTest");
         LoginPage loginPage = new LoginPage(driver);
         loginPage.visit();
         
         // Type username incorrectUser into Username field        
         // Type password Password123 into Password field
         //  Push Submit button
-        loginReport.info("Type username, password, and click submit button");
+        testReport.info("Type username, password, and click submit button");
         loginPage.executeLogin(username, password);
         
         //  Verify error message is displayed
         //  Verify error message text is Your username is
         Assert.assertEquals(loginPage.getErrorMessage(), expectedErrorMessage);
-        loginReport.pass("Test passed...");
+        testReport.pass("Test passed...");
         extent.flush();
     }
 }
